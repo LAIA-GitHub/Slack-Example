69518e2a3f2b800047b32ff2ec12b69afb6e9444
marius-schairer <147055411+marius-schairer@users.noreply.github.com>
2024-07-06 07:30:48 +0200

    create vector

--- functions/CreateVector.py
+++ functions/CreateVector.py
@@ -10,7 +10,7 @@ from langchain_community.vectorstores.faiss import FAISS
 def create_vector_store(docs):
     embedding = OpenAIEmbeddings()
     vector_store = FAISS.from_documents(docs, embedding=embedding)
-    vector_store.save_local('../docs/static')
+    vector_store.save_local('docs/static')
     return vector_store
 
 def load_vector_store(store_path: str) -> FAISS:
